32cd81c5d392a3862682b29696733dc0bbc18615
sablest <dmitriy.sobolev@sablest.com>
2025-11-05 12:41:17 +0400

    fix: Rename next.config.ts to next.config.js for Next 14

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Здесь мы будем добавлять настройки в будущем, если понадобятся
 };
 
-export default nextConfig;
+module.exports = nextConfig;
